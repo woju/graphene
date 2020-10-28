1b079ef009fa2c3ccede88b79694052f11b2aff2
Wojtek Porczyk <woju@invisiblethingslab.com>
2020-10-28 17:03:42 +0100

    fixup! tests: Add basic benchmarks

--- tests/benchmarks/basic.py
+++ tests/benchmarks/basic.py
@@ -2,28 +2,28 @@ from . import Exec
 
 # pylint: disable=invalid-name
 
-helloworld = Exec('helloworld', manifest_template='basic.manifest.template')
-@helloworld.add_setup
-def time_000_helloworld():
-    helloworld.run_in_graphene()
-time_000_helloworld.setup = helloworld.setup
+class HelloWorld:
+    # pylint: disable=no-self-use
 
-write_1e4 = Exec('write_1e4', manifest_template='basic.manifest.template')
-@write_1e4.add_setup
-def time_014_write_1e4():
-    write_1e4.run_in_graphene()
+    helloworld = Exec('helloworld', manifest_template='basic.manifest.template')
+    setup = helloworld.setup
 
-write_1e5 = Exec('write_1e5', manifest_template='basic.manifest.template')
-@write_1e5.add_setup
-def time_015_write_1e5():
-    write_1e5.run_in_graphene()
+    def time_graphene_nosgx(self):
+        self.helloworld.run_in_graphene(sgx=False)
 
-write_1e6 = Exec('write_1e6', manifest_template='basic.manifest.template')
-@write_1e6.add_setup
-def time_016_write_1e6():
-    write_1e6.run_in_graphene()
+    def time_graphene_sgx(self):
+        self.helloworld.run_in_graphene(sgx=True)
 
-write_1e7 = Exec('write_1e7', manifest_template='basic.manifest.template')
-@write_1e7.add_setup
-def time_017_write_1e7():
-    write_1e7.run_in_graphene()
+class WritePages:
+    # pylint: disable=no-self-use
+
+    write_pages = Exec('write_pages', manifest_template='basic.manifest.template')
+    params = [10000, 100000, 1000000, 10000000]
+    param_namess = ['pagecount']
+    setup = write_pages.setup
+
+    def time_graphene_nosgx(self, pagecount):
+        self.write_pages.run_in_graphene(str(pagecount), sgx=False)
+
+    def time_graphene_sgx(self, pagecount):
+        self.write_pages.run_in_graphene(str(pagecount), sgx=True)
